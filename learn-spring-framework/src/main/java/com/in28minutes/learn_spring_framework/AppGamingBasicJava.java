3b4b3da3dd11c8198a70fadb5de7fcaa24c47d2d
ljh37694 <ljh37694@naver.com>
2024-12-11 23:04:43 +0900

    init

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/AppGamingBasicJava.java
@@ -0,0 +1,13 @@
+package com.in28minutes.learn_spring_framework;
+
+import com.in28minutes.learn_spring_framework.game.GameRunner;
+import com.in28minutes.learn_spring_framework.game.MarioGame;
+
+public class AppGamingBasicJava {
+	public static void main(String[] args) {
+		MarioGame marioGame = new MarioGame();
+		GameRunner gameRunner = new GameRunner(marioGame);
+		
+		gameRunner.run();
+	}
+}

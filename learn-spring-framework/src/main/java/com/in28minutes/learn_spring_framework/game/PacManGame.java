3be9616453f011dc00d012dd957e5f0cce65db59
ljh37694 <ljh37694@naver.com>
2024-12-12 23:29:55 +0900

    learn tight coupling and loose coupling

---
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/game/PacManGame.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learn_spring_framework.game;
+
+public class PacManGame implements GamingConsole {
+	public void up() {
+		System.out.println("eat up");
+	}
+	
+	public void down() {
+		System.out.println("eat down");
+	}
+	
+	public void left() {
+		System.out.println("eat left");
+	}
+	
+	public void right() {
+		System.out.println("eat right");
+	}
+}

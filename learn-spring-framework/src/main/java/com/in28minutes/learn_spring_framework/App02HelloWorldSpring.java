fa3ea6ccdd32f698e398eb1ade5ee96faddade2e
i_junghoon <ljh37694@naver.com>
2024-12-13 19:41:07 +0900

    Bean을 parameter 또는 MethodCall 해서 만들기

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -8,5 +8,10 @@ public class App02HelloWorldSpring {
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("customAddress"));
+		System.out.println(context.getBean(Address.class));
+		System.out.println(context.getBean("personMethodCall"));
+		System.out.println(context.getBean("personParameter"));
 	}
 }
--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,10 +3,39 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {}
+
+record Address(String firstLine, String city) {}
+
 @Configuration
 public class HelloWorldConfiguration {
 	@Bean
 	public String name() {
 		return "Mikasa";
+	}
+	
+	@Bean
+	public int age() {
+		return 23;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("minsu", 10);
+	}
+	
+	@Bean
+	public Person personMethodCall() {
+		return new Person(name(), age());
+	}
+	
+	@Bean
+	public Person personParameter(String name, int age) {
+		return new Person(name, age);
+	}
+	
+	@Bean(name = "customAddress")
+	public Address address() {
+		return new Address("경기도", "광주");
 	}
 }

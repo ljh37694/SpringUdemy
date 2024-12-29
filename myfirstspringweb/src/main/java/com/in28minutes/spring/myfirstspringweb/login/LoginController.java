0f58eb1798723b46aff0089272bf6df92d7054e5
ljh37694 <ljh37694@naver.com>
2024-12-29 18:16:42 +0900

    logging 실습

--- myfirstspringweb/src/main/java/com/in28minutes/spring/myfirstspringweb/login/LoginController.java
+++ myfirstspringweb/src/main/java/com/in28minutes/spring/myfirstspringweb/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.spring.myfirstspringweb.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("login")
 	public String login(@RequestParam String name, ModelMap model) {
 		model.put("name", name);
+		
+		logger.debug("Request name is {}", name);
 		System.out.println(name);
 		
 		return "login";

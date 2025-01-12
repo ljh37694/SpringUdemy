270f120492519452d98881b30a823e1fc03ed03d
ljh37694 <ljh37694@naver.com>
2025-01-12 23:12:28 +0900

    Todo Entity 만들고 h2에 넣기

--- myfirstspringweb/src/main/java/com/in28minutes/spring/myfirstspringweb/todo/Todo.java
+++ myfirstspringweb/src/main/java/com/in28minutes/spring/myfirstspringweb/todo/Todo.java
@@ -2,14 +2,19 @@ package com.in28minutes.spring.myfirstspringweb.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Todo {
-	
+	@Id
+	@GeneratedValue
 	private int id;
 	private String username;
 	
-	@Size(min=10, message="Enter at least 10 characters")
+	@Size(min=5, message="Enter at least 10 characters")
 	private String description;
 	private LocalDate targetDate;
 	private boolean done;
---
+++ myfirstspringweb/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001, 'Lee', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002, 'Lee', 'Get React Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003, 'Lee', 'Get NextJs Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004, 'Lee', 'Get ReactNative Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10005, 'Lee', 'Get AWS Certified', CURRENT_DATE(), false);

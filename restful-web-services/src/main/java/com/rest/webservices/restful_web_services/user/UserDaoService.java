2e8e00a8c75deb4259708692c86dbf8eb99c1068
i_junghoon <ljh37694@naver.com>
2025-01-15 14:07:31 +0900

    User DAO service 생성

---
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -0,0 +1,23 @@
+package com.rest.webservices.restful_web_services.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+//Data Access Object
+@Component
+public class UserDaoService {
+	 private static List<User> users = new ArrayList<>();
+	 
+	 static {
+		 users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+		 users.add(new User(2, "Eve", LocalDate.now().minusYears(22)));
+		 users.add(new User(3, "Lee", LocalDate.now().minusYears(24)));
+	 }
+	 
+	 public List<User> findAll() {
+		 return users;
+	 }
+}

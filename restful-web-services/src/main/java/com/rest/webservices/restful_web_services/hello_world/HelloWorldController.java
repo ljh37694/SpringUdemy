1f385d61fadc41dae2552ae82174c9dc285cf728
i_junghoon <ljh37694@naver.com>
2025-01-15 13:37:25 +0900

    path variable 실습

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/hello_world/HelloWorldController.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/hello_world/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rest.webservices.restful_web_services.hello_world;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,10 @@ public class HelloWorldController {
 	@GetMapping("hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World Bean!");
+	}
+	
+	@GetMapping("hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World %s!", name));
 	}
 }

4a5da0f46f4074317688b76ff1aed3bc1726d7cb
ljh37694 <ljh37694@naver.com>
2025-01-27 12:51:01 +0900

    json static filtering 실습

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/User.java
@@ -2,6 +2,8 @@ package com.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -9,9 +11,11 @@ public class User {
 	private Integer id;
 	
 	@Size(min = 2, message = "Name shoule have at least 2 characters")
+	@JsonProperty("user_name")
 	private String name;
 	
 	@Past(message = "Birst Date should be in the past")
+	@JsonProperty("birth_date")
 	private LocalDate birthDate;
 	
 	public User(Integer id, String name, LocalDate birthDate) {

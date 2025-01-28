87a46bfcde172b28fe1b0cc41e295095cb35caf7
ljh37694 <ljh37694@naver.com>
2025-01-28 15:08:47 +0900

    User Entity와 일대다 관계인 Post Entity 생성 및 데이터 추가

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/user/User.java
@@ -1,12 +1,15 @@
 package com.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -27,6 +30,10 @@ public class User {
 	@Past(message = "Birst Date should be in the past")
 	@JsonProperty("birth_date")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<Post> posts;
 	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -6,3 +6,12 @@ values(10002, current_date(), 'Park');
 
 insert into user_details(id,birth_date,name)
 values(10003, current_date(), 'Baek');
+
+insert into post(id, description, user_id)
+values(20001, 'I want to learn React', 10001);
+
+insert into post(id, description, user_id)
+values(20002, 'I want to learn Nest', 10002);
+
+insert into post(id, description, user_id)
+values(20003, 'I want to learn Java', 10002);

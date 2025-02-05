3d20810f65ae16f480f11ca1a75794c73b9a628c
i_junghoon <ljh37694@naver.com>
2025-02-05 13:58:27 +0900

    login component 생성성

--- react/todo-app/src/components/todo/TodoApp.jsx
+++ react/todo-app/src/components/todo/TodoApp.jsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
 function TodoApp() {
   return (
     <div className="todo-app">
       <h1>Todo App</h1>
+      <LoginComp />
+    </div>
+  );
+}
+
+function LoginComp() {
+  const [username, setUsername] = useState("Lee");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="login-comp">
+      <div>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+
+        <button>login</button>
+      </div>
     </div>
   );
 }

e46da80dd043c6ca4d989eca1011ac818b63675a
i_junghoon <ljh37694@naver.com>
2025-02-06 13:53:48 +0900

    react router dom 설치 및 실습

--- react/todo-app/src/components/todo/TodoApp.jsx
+++ react/todo-app/src/components/todo/TodoApp.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
 
 function TodoApp() {
   return (
     <div className="todo-app">
       <h1>Todo App</h1>
-      <LoginComp />
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginComponent />} />
+          <Route path="/login" element={<LoginComponent />} />
+          <Route path="/welcome" element={<WelcomeComponent />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-function LoginComp() {
+function LoginComponent() {
   const [username, setUsername] = useState("Lee");
   const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   return (
     <div className="login-comp">
@@ -25,8 +35,16 @@ function LoginComp() {
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
 
-        <button>login</button>
+        <button onClick={() => navigate('/welcome')}>login</button>
       </div>
+    </div>
+  );
+}
+
+function WelcomeComponent() {
+  return (
+    <div className="welcome-comp">
+      <h2>Welcome Lee</h2>
     </div>
   );
 }

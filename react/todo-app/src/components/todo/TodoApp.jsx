30d8254c6554a78f14ed9ea7228d0f1a3c5a19d4
ljh37694 <ljh37694@naver.com>
2025-02-08 18:31:41 +0900

    welcome 페이지 하드코딩 개선 및 TodoListComponent 추가

--- react/todo-app/src/components/todo/TodoApp.jsx
+++ react/todo-app/src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useNavigate, useParams } from "react-router-dom";
 
 function TodoApp() {
   return (
@@ -10,7 +10,9 @@ function TodoApp() {
         <Routes>
           <Route path="/" element={<LoginComponent />} />
           <Route path="/login" element={<LoginComponent />} />
-          <Route path="/welcome" element={<WelcomeComponent />} />
+          <Route path="/welcome/:username" element={<WelcomeComponent />} />
+          <Route path="/todos" element={<TodoListComponent />} />
+
           <Route path="*" element={<ErrorComponent />} />
         </Routes>
       </BrowserRouter>
@@ -36,16 +38,18 @@ function LoginComponent() {
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
 
-        <button onClick={() => navigate('/welcome')}>login</button>
+        <button onClick={() => navigate('/welcome/' + username)}>login</button>
       </div>
     </div>
   );
 }
 
 function WelcomeComponent() {
+  const { username } = useParams();
+
   return (
     <div className="welcome-comp">
-      <h2>Welcome Lee</h2>
+      <h2>Welcome {username}</h2>
     </div>
   );
 }
@@ -56,6 +60,36 @@ function ErrorComponent() {
       <h2>404 - Page not found</h2>
     </div>
   );
+}
+
+function TodoListComponent() {
+  const todoList = [
+    { id: 1, description: "Learn React", done: false, targetDate: new Date() },
+  ];
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Done</th>
+            <th>Target Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todoList.map(todo => (
+            <tr key={todo.id}>
+              <td>{todo.description}</td>
+              <td>{todo.done.toString()}</td>
+              <td>{todo.targetDate.toString()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default TodoApp;

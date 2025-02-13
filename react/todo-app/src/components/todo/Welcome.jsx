53dcdbe542726c348c5658c2e37450e1180ef068
ljh37694 <ljh37694@naver.com>
2025-02-13 20:27:09 +0900

    axios 설치 및 CORS 설정

--- react/todo-app/src/components/todo/Welcome.jsx
+++ react/todo-app/src/components/todo/Welcome.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { Link, useParams } from "react-router-dom";
 
 function WelcomeComponent() {
@@ -8,6 +9,16 @@ function WelcomeComponent() {
       <h2>Welcome {username}</h2>
 
       <p>Manage your todos - <Link to="/todos">Go here</Link></p>
+
+      <button onClick={() => {
+        axios.get("http://localhost:8080/hello-world", {
+          withCredentials: true,
+        })
+          .then((res) => {
+            console.log(res.data);
+          })
+          .catch(e => console.error(e));
+      }}>Call Hello World</button>
     </div>
   );
 }

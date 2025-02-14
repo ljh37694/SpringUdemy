b455fa2970090cf8050eb1e34a2255ffc9cf4351
i_junghoon <ljh37694@naver.com>
2025-02-14 13:47:49 +0900

    Todo GET,DELETE api 생성

--- restful-web-services/src/main/java/com/rest/webservices/restful_web_services/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/rest/webservices/restful_web_services/todo/TodoResource.java
@@ -2,7 +2,9 @@ package com.rest.webservices.restful_web_services.todo;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,17 @@ public class TodoResource {
 	@GetMapping(path = "/users/{username}/todo-list")
 	public List<Todo> retrieveTodoList(@PathVariable String username) {
 		return todoService.findByUsername(username);
+	}
+	
+	@GetMapping("/users/{username}/todo-list/{id}")
+	public Todo retrieveTodo(@PathVariable String username, @PathVariable int id) {
+		return todoService.findById(id);
+	}
+	
+	@DeleteMapping("/user/{username}/todo-list/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable int id) {
+		todoService.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
